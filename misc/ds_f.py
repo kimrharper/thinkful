2665719191cf84b2a05c1ef39684d5357fc48fc3
kimrharper <kimrharper@gmail.com>
2018-11-05 10:58:28 -0800

    added linalg methods to misc folder

--- misc/ds_f.py
+++ misc/ds_f.py
@@ -1,4 +1,8 @@
 import math
+from collections import Counter
+from functools import partial,reduce
+
+# linalg
 
 """vector addition"""
 def vector_add(v, w):
@@ -16,8 +20,7 @@ def vector_sum(vectors):
     return result
 
 """Alternative method for vector summation"""
-from functools import partial,reduce
-vector_sum = partial(reduce, vector_add)
+# vector_sum = partial(reduce, vector_add)
 
 """vector scaling"""
 def scalar_multiply(c,v):
@@ -45,3 +48,60 @@ def squared_distance(v,w):
 def distance(v,w):
     return magnitude(vector_subtract(v,w))
 
+
+# stats
+
+def mean(x):
+    return sum(x)/len(x)
+
+def median(v):
+    n=len(v)
+    sorted_v = sorted(v)
+    midpoint = n//2
+    
+    if n%2 ==1:
+        return sorted_v[midpoint]
+    else:
+        lo=midpoint-1
+        hi=midpoint
+        return (sorted_v[lo]+sorted_v[hi]/2)
+    
+def quantile(x,p):
+    p_index = int(p*len(x))
+    return sorted(x)[p_index]
+
+def mode(x):
+    counts=Counter(x)
+    max_count=counts.most_common(1)[0][1]
+    most_common = [m[0] for m in counts.items() if m[1]==max_count]
+    return [x_i for x_i in x if x_i in most_common] if (len(most_common)==1 and max_count>1) else None
+
+def data_range(x):
+    return max(x)-min(x)
+
+def de_mean(x):
+    x_bar = mean(x)
+    return [x_i-x_bar for x_i in x]
+
+def variance(x):
+    n=len(x)
+    deviations=de_mean(x)
+    return sum_of_squares(deviations) /(n-1)
+
+def standard_deviation(x):
+    return math.sqrt(variance(x))
+
+def interquartile_range(x):
+    return quantile(x,.75)- quantile(x,.25)
+
+def covariance(x,y):
+    n=len(x)
+    return (dot(de_mean(x),de_mean(y)))/(n-1)
+
+def correlation(x,y):
+    stdev_x = standard_deviation(x)
+    stdev_y = standard_deviation(y)
+    if stdev_x > 0 and stdev_y > 0:
+        return covariance(x,y)/stdev_x/stdev_y
+    else:
+        return 0
